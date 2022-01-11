7daff27d6eca21d45dbb3fa828d8929c1772caa5
Adam Becker <ad@mbecker.cc>
2022-01-10 22:03:44 -0800

    csv, more ignore list

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const fs = require('fs')
+const path = require('path')
 const chalk = require('chalk')
 const ethers = require('ethers')
 const filter = require('lodash.filter')
@@ -5,12 +7,14 @@ const reduce = require('lodash.reduce')
 const uniq = require('lodash.uniq')
 const compact = require('lodash.compact')
 
-const START_BLOCK = 23432417
-const END_BLOCK = 23432517
+const START_BLOCK = parseInt(process.env.START_BLOCK, 10)
+const END_BLOCK = parseInt(process.env.END_BLOCK, 10)
 const SPAM_CUTOFF = 5
 
 const OK_LIST = [
-  '0x2953399124F0cBB46d2CbACD8A89cF0599974963' // opensea
+  '0x2953399124F0cBB46d2CbACD8A89cF0599974963', // opensea
+  '0xdf9B4b57865B403e08c85568442f95c26b7896b0', // sunflower
+  '0xa5E0829CaCEd8fFDD4De3c43696c57F7D7A678ff' // qs router
 ]
 
 async function main () {
@@ -43,17 +47,27 @@ async function main () {
     }
 
     const spamRate = spam > 0 ? Math.round(spam / total * 100) : 0
-    spamRates.push(spamRate)
+    spamRates.push({ block: i, rate: spamRate, spam, total })
     console.log(chalk.bold(`Block ${i}, found ${spam} spam txs, ${total} total transactions. Spam rate ${spamRate}%`))
     console.log('')
   }
 
   provider._websocket.close()
 
-  const sum = reduce(spamRates, function (sum, a) { return sum + a }, 0)
+  const sum = reduce(spamRates, function (sum, a) { return sum + a.rate }, 0)
   const avg = Math.round(sum / spamRates.length)
   console.log('')
   console.log(chalk.bold(`Analyzed ${spamRates.length} blocks. Average spam rate ${avg}%`))
+  console.log('')
+  console.log('')
+
+  const csvArr = ['block,rate,spam,total']
+
+  for (const s of spamRates) {
+    csvArr.push(`${s.block},${s.rate},${s.spam},${s.total}`)
+  }
+
+  fs.writeFileSync(path.join(__dirname, './out.csv'), csvArr.join('\n'), 'utf-8')
 }
 
 main()
